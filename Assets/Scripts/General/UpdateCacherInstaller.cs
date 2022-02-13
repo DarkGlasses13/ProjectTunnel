b7090d78528843982f2675b06030021acb9e692e
Matroskin_Cat <groznov3@gmail.com>
2022-02-13 17:57:52 +0300

    InitProject

---
+++ Assets/Scripts/General/UpdateCacherInstaller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Assets.Scripts
+{
+    [RequireComponent(typeof(UpdateCacher))]
+    public class UpdateCacherInstaller : MonoInstaller
+    {
+        private UpdateCacher _updateCacher;
+
+        public override void InstallBindings()
+        {
+            _updateCacher = GetComponent<UpdateCacher>();
+
+            Container.
+                Bind<UpdateCacher>().
+                FromInstance(_updateCacher).
+                AsSingle();
+        }
+    }
+}
