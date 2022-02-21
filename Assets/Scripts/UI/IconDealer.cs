ef342265e7a2e2d24101a8a745f03bf9e83c7076
Evgeniy Polyakov <vip.zhenya.polyakov.00@mail.ru>
2022-02-21 22:21:25 +0300

    HubUpdate

--- Assets/Scripts/UI/IconDealer.cs
+++ Assets/Scripts/UI/IconDealer.cs
@@ -6,13 +6,17 @@ namespace Assets.Scripts
     public class IconDealer : MonoBehaviour
     {
         [SerializeField] int _itemPull;
+        [SerializeField] Transform _viewParent;
         [SerializeField] CharacterConfig _config;
 
         private Dictionary<int, EquipmentView> _shopsAssortment;
         private EquipmentView _equipedWeapon;
 
+        private Transform _hand;
+
         public void Init(Transform hand, Transform iconParent, Icon iconPrefab, ItemDatabase database)
         {
+            _hand = hand;
             _shopsAssortment = new Dictionary<int, EquipmentView>(_itemPull);
             for (int i = 0; i < _itemPull; i++)
             {
@@ -22,7 +26,7 @@ namespace Assets.Scripts
                 icon.OnClick += ChangeWeapon;
                 icon.SettingIcon(weapon);
 
-                CreateItemView(hand, weapon);
+                CreateItemView(weapon);
             }
         }
 
@@ -30,16 +34,22 @@ namespace Assets.Scripts
         {
             if (_equipedWeapon != null)
             {
+                _equipedWeapon.transform.position = _viewParent.position;
+                _equipedWeapon.transform.SetParent(_viewParent);
                 _equipedWeapon.gameObject.SetActive(false);
             }
             _equipedWeapon = _shopsAssortment[weapon.ID];
+
+            _equipedWeapon.transform.position = _hand.position;
+            _equipedWeapon.transform.SetParent(_hand);
             _equipedWeapon.gameObject.SetActive(true);
+
             _config.SetEquipedWeapon(weapon);
         }
 
-        void CreateItemView(Transform hand, Weapon weapon)
+        void CreateItemView(Weapon weapon)
         {
-            EquipmentView equipment = Instantiate(weapon.Prefab, hand);
+            EquipmentView equipment = Instantiate(weapon.Prefab, _viewParent);
             _shopsAssortment.Add(weapon.ID, equipment);
 
             equipment.gameObject.SetActive(false);
